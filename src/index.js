3cf9ec262c0716ed0328ecf8aa619128f840b68a
Alex <alexzloid@gmail.com>
2023-08-18 07:06:46 +0500

    feat: create resolve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+	if (!matrix || matrix.length === 0) return [];
+	const res = matrix.reduce((acc, cur, i) => {
+		cur.sort((a, b) => (!(i & 1) ? a - b : b - a)).map(e => acc.push(e));
+		return acc;
+	}, []);
+	return res;
+};
